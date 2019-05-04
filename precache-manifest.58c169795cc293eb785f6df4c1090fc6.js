5699ce9cf689b99b23722cdc8e24ae90a9699f66
enigmaeth <enigmaeth@gmail.com>
2019-05-04 16:22:29 +0530

    Updates

---
+++ precache-manifest.58c169795cc293eb785f6df4c1090fc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6adebdfcfe296c1a7a6d",
+    "url": "/style-transfer/static/js/runtime~main.6adebdfc.js"
+  },
+  {
+    "revision": "566065b24390b3997a00",
+    "url": "/style-transfer/static/js/main.566065b2.chunk.js"
+  },
+  {
+    "revision": "9cc6368eec602fad1854",
+    "url": "/style-transfer/static/js/1.9cc6368e.chunk.js"
+  },
+  {
+    "revision": "566065b24390b3997a00",
+    "url": "/style-transfer/static/css/main.48c1445e.chunk.css"
+  },
+  {
+    "revision": "c88751a55ce637b6167139273f1c9c93",
+    "url": "/style-transfer/index.html"
+  }
+];
